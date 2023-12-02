a88cd37ce6211a3145444cd853810e94420cffaa
Magister <xqjh@mail.ru>
2023-12-02 21:48:00 +0300

    Добавлен корневой фрагмент Добавлен навигационный граф В корневой фрагмент вставлен view pager

---
+++ app/src/main/java/mars/marsweather/RootFragment.kt
@@ -0,0 +1,43 @@
+package mars.marsweather
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import mars.marsweather.databinding.FragmentRootBinding
+
+class RootFragment : Fragment() {
+    private var _binding: FragmentRootBinding? = null
+    private val binding: FragmentRootBinding
+        get() = _binding ?: throw Exception("")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRootBinding.inflate(inflater, container, false)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager() {
+        val adapter = RootFragmentRecycler(this, arrayListOf(StartFragment()))
+        binding.viewPager.adapter = adapter
+    }
+
+    class RootFragmentRecycler(
+        fragment: Fragment,
+        private val fragmentList: ArrayList<Fragment>
+    ) : FragmentStateAdapter(fragment) {
+        override fun getItemCount() = fragmentList.size
+        override fun createFragment(position: Int) = fragmentList[position]
+    }
+}
