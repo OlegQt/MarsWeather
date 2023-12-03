c068da084506a6faab03a27c138260aae68c3157
Magister <xqjh@mail.ru>
2023-12-03 23:35:27 +0300

    Переход на Material3

--- app/src/main/java/mars/marsweather/StartFragment.kt
+++ app/src/main/java/mars/marsweather/StartFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import mars.marsweather.databinding.FragmentRootBinding
 
 class StartFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +17,20 @@ class StartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
+
+class DefaultFragment<T:ViewBinding> ():Fragment(){
+
+    lateinit var x:(LayoutInflater,ViewGroup?,Boolean)->T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val k = x(inflater,container,false)
+        return k.root
+        //return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
