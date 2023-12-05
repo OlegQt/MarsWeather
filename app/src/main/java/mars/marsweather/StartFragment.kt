f066419015d7af11d7cc669c6a86e0b7856bb592
Oleg note <xqjh@mail.ru>
2023-12-05 18:24:11 +0300

    ViewModel for StartFragment

--- app/src/main/java/mars/marsweather/StartFragment.kt
+++ app/src/main/java/mars/marsweather/StartFragment.kt
@@ -1,31 +1,48 @@
 package mars.marsweather
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.snackbar.Snackbar
-import mars.marsweather.databinding.FragmentRootBinding
 
 class StartFragment : Fragment() {
+    private var _vm: StartVm? = null
+    private val vm:StartVm get() = _vm ?: throw Exception("Start viewModel is null")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _vm = ViewModelProvider(this)[StartVm::class.java]
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // For ViewModel
+        setObservers()
+
         view.findViewById<Button>(R.id.btn_action).setOnClickListener {
-            Snackbar.make(it, "Click", Snackbar.LENGTH_SHORT)
-                .setTextColor(resources.getColor(R.color.dark_primary, requireContext().theme))
-                .show()
+            showSnack("Click")
         }
+    }
+
+    private fun setObservers() {
+        vm.state.observe(viewLifecycleOwner){
+            showSnack(it)
+        }
+    }
+
+    private fun showSnack(txt:String){
+        Snackbar.make(requireView(), txt, Snackbar.LENGTH_SHORT)
+            .setTextColor(resources.getColor(R.color.dark_primary, requireContext().theme))
+            .show()
     }
 }
 
@@ -37,7 +54,7 @@ class DefaultFragment<T : ViewBinding>() : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val k = x(inflater, container, false)
         return k.root
         //return super.onCreateView(inflater, container, savedInstanceState)
--- app/src/main/java/mars/marsweather/StartVm.kt
+++ app/src/main/java/mars/marsweather/StartVm.kt
@@ -1,4 +1,14 @@
 package mars.marsweather
 
-class StartVm {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StartVm:ViewModel() {
+    private val _state = MutableLiveData<String>()
+    val state = _state as LiveData<String>
+
+    init {
+        _state.value = "Init"
+    }
 }
