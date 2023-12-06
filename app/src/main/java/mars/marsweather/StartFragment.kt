742827c5a9cec20828b3a4f9dbadb2ff0451d3fd
OlegWork <xqjh@mail.ru>
2023-12-06 14:42:45 +0300

    DataPicker button style changed

--- app/src/main/java/mars/marsweather/StartFragment.kt
+++ app/src/main/java/mars/marsweather/StartFragment.kt
@@ -1,11 +1,14 @@
 package mars.marsweather
 
+import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
@@ -14,9 +17,9 @@ import com.google.android.material.snackbar.Snackbar
 
 class StartFragment : Fragment() {
     private var _vm: StartVm? = null
-    private val vm:StartVm get() = _vm ?: throw Exception("Start viewModel is null")
+    private val vm: StartVm get() = _vm ?: throw Exception("Start viewModel is null")
 
-    private var image:ImageView? = null
+    private var image: ImageView? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,20 +35,38 @@ class StartFragment : Fragment() {
         // For ViewModel
         setObservers()
 
-        image =  view.findViewById(R.id.mars_photo)
+        image = view.findViewById(R.id.mars_photo)
 
         view.findViewById<Button>(R.id.btn_action).setOnClickListener {
             vm.loadPhotoFromMars()
         }
+
+        view.findViewById<TextView>(R.id.txt_info).setOnClickListener {
+            openDatePicker()
+        }
+    }
+
+    private fun openDatePicker() {
+        val dateDlg = DatePickerDialog(requireContext())
+        dateDlg.setOnDateSetListener { datePicker, year, month, day ->
+            val strDate = with(StringBuilder()) {
+                append("Year $year \n")
+                append("month $month \n")
+                append("day $day")
+            }.toString()
+            showSnack(strDate)
+        }
+        dateDlg.setMessage("Message")
+        dateDlg.show()
     }
 
     private fun setObservers() {
-        vm.state.observe(viewLifecycleOwner){
+        vm.state.observe(viewLifecycleOwner) {
             showSnack(it)
         }
 
-        vm.imageMarsUrl.observe(viewLifecycleOwner){
-            if (it.isNotEmpty()){
+        vm.imageMarsUrl.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
                 Glide
                     .with(this)
                     .load(it)
@@ -55,25 +76,11 @@ class StartFragment : Fragment() {
         }
     }
 
-    private fun showSnack(txt:String){
-        Snackbar.make(requireView(), txt, Snackbar.LENGTH_SHORT)
+    private fun showSnack(txt: String) {
+        Snackbar.make(requireView(), txt, Snackbar.LENGTH_INDEFINITE)
             .setTextColor(resources.getColor(R.color.dark_primary, requireContext().theme))
             .setTextMaxLines(20)
+            .setAction("OK") { }
             .show()
-    }
-}
-
-class DefaultFragment<T : ViewBinding>() : Fragment() {
-
-    lateinit var x: (LayoutInflater, ViewGroup?, Boolean) -> T
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val k = x(inflater, container, false)
-        return k.root
-        //return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
